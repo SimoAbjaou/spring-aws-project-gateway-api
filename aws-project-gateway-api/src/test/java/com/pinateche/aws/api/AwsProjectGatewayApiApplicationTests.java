29f5bae6f554f51b51a0c23e80f8becb3280c481
SimoAbjaou <mo.abjaou@gmail.com>
2020-10-18 15:52:38 +0100

    first commit

---
+++ aws-project-gateway-api/src/test/java/com/pinateche/aws/api/AwsProjectGatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinateche.aws.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProjectGatewayApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
